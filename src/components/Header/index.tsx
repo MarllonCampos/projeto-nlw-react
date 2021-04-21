323880f4ab6be57c58f03e4190e23e00dda31b8f
marlloncampos <marllondcsp@gmail.com>
2021-04-21 08:52:03 -0300

    Criado o header da aplicação e sua estilização

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,19 @@
-
+import format from 'date-fns/format'
+import ptBR from 'date-fns/locale/pt-BR'
+import styles from './styles.module.scss'
 
 export function Header() {
+    const currentDate = format(new Date(), 'EEEEEE,d MMMM', {
+        locale:ptBR
+    } );
+
     return(
-        <h1>Header</h1>
+        <header className={styles.headerContainer}>
+            <img src="/logo.svg" alt="logo" />
+
+            <p>O melhor para você ouvir sempre</p>
+
+            <span>{currentDate}</span>
+        </header>
     );
 }
